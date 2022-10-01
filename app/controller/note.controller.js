1a82c9e908b6daca3ec90db2bd7c83628d5bb77b
Mohsin <razam305@gmail.com>
2022-10-01 18:54:32 +0530

    creating a new note

--- app/controller/note.controller.js
+++ app/controller/note.controller.js
@@ -53,6 +53,31 @@ exports.update = (req, res) => {
                 message: "Error updating note with id " + req.params.noteId
             });
         });
+        // Create and Save a new Note
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+};
     // Retrieve and return all notes from the database.
     exports.findAll = (req, res) => {
         Note.find()
