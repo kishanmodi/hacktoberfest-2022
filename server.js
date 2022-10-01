cefaabb0ac480c750c7bc18ed446fe61a8ac6aac
Sudhan <sudhanplayz@gmail.com>
2022-10-01 11:40:20 +0000

    Fix #5

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("successfully connected to the database");    
+}).catch(err => {
+    console.log('could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 app.get('/', (req, res) => {
     res.json({"message": "app is working."});
 });
